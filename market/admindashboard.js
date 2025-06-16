ade426a0a313089a2b44774314e3b5b69e3b8be4
Laurente, Jahmiebelle P. <jahmiebelle.laurente@cvsu.edu.ph>
2025-06-16 23:46:39 +0800

    Delete market directory

--- market/admindashboard.js
+++
@@ -1,4 +0,0 @@
-function showSection(sectionId) {
-    document.querySelectorAll('.section').forEach(sec => sec.classList.remove('active'));
-    document.getElementById(sectionId).classList.add('active');
-}
